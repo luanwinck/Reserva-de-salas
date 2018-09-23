f1ad7aa0fc0ad86b8b4b28ea7715d9937e4f3af5
Luan Winck <luan.winck@cwi.com.br>
2018-09-23 20:07:39 -0300

    ghome

--- front/src/scenes/Home/index.jsx
+++ front/src/scenes/Home/index.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom'
 
 import { Button, Input } from 'reactstrap'
+import Select from '../../components/generic/Select/index'
 
 import './style.css'
 
@@ -38,6 +39,24 @@ validateDate() {
 
 }
 
+getCategoriesOptions() {
+    return [
+        {
+            value: 'Sala de reunião',
+            text: 'Sala de reunião'
+        },
+        {
+            value: 'Sala 102',
+            text: 'Sala 102'
+        },
+        {
+            value: 'Sala 103',
+            text: 'Sala 103'
+        }
+    ]
+}
+
+
 
 render() {
 if (this.state.shouldRedirectLogin) {
@@ -47,24 +66,39 @@ if (this.state.shouldRedirectLogin) {
 return (
     <div>
         <h1>Home</h1>
-        {/* <Button color='danger' onClick={this._logout}>Logout</Button> */}
+        <Button color='danger' onClick={this._logout}>Logout</Button>
         <div className="container-home">
-        <span>Data inicial</span>
-        <Input
-            value={this.state.dataInicial}
-            name="dataInicial"
-            onChange={this.handleChange}
-            type="datetime-local"
-        />
-        <span>Data final</span>
-        <Input
-            value={this.state.dataFinal}
-            name="dataFinal"
-            onChange={this.handleChange}
-            type="datetime-local"
-        />
+            <span>Data</span>
+            <Input
+                value={this.state.dataInicial}
+                name="dataInicial"
+                onChange={this.handleChange}
+                type="date"
+            />
+            <span>Hora inicial</span>
+            <Input
+                value={this.state.dataFinal}
+                name="dataFinal"
+                onChange={this.handleChange}
+                type="time"
+            />
+            <span>Hora final</span>
+            <Input
+                value={this.state.dataFinal}
+                name="dataFinal"
+                onChange={this.handleChange}
+                type="time"
+            />
+            <Select 
+                label="Salas disponíveis"
+                name="categoria"
+                value={this.state.categoria}
+                options={this.getCategoriesOptions()}
+                handdleChange={this.handdleChange} 
+            />
+            <span>Descrição da reserva</span>
+            <Input type="textarea" name="descricao" />
         </div>
-        <Button color='primary' onClick={this.validateDate}>Data</Button>
     </div>
 );
   }
